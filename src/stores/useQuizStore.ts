23a3308862fa1131681484a75498a98ae0487c70
KotoFeelGood72 <kotenkolove@yandex.ru>
2024-10-22 12:22:45 +0300

    ыы

--- src/stores/useQuizStore.ts
+++ src/stores/useQuizStore.ts
@@ -3,16 +3,16 @@ import { defineStore, storeToRefs } from "pinia";
 export const useQuizStore = defineStore("quiz", {
   state: () => ({
     quiz: [] as any[],
-    currentStep: 1, 
+    currentStep: 1,
     selectedOptions: [] as any[],
     steps: [
       {
         title: "Есть ли у вас готовый проект?",
         options: [
-          { title: "Есть", image: '' },
-          { title: "Нет", image: '' },
-          { title: "Выберу из готовых вариантов", image: '' },
-          { title: "Хочу заказать индивидуальный проект", image: '' },
+          { title: "Есть", image: "" },
+          { title: "Нет", image: "" },
+          { title: "Выберу из готовых вариантов", image: "" },
+          { title: "Хочу заказать индивидуальный проект", image: "" },
         ],
       },
       {
@@ -20,30 +20,32 @@ export const useQuizStore = defineStore("quiz", {
         options: [
           {
             title: "Дом с классической крышей",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
           {
             title: "Дом с плоской крышей",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
         ],
       },
       {
         title: "Сколько этажей вы желаете:",
         options: [
-          { title: "1", image: '' },
-          { title: "2", image: '' },
-          { title: "1 + Мансарда", image: '' },
-          { title: "2 + Мансарда", image: '' },
+          { title: "1", image: "" },
+          { title: "2", image: "" },
+          { title: "1 + Мансарда", image: "" },
+          { title: "2 + Мансарда", image: "" },
         ],
       },
       {
         title: "Какую площадь дома вы планируете?",
         options: [
-          { title: "до 100 м²", image: '' },
-          { title: "от 100 м² до 200 м²", image: '' },
-          { title: "от 200 м² до 300 м²", image: '' },
-          { title: "свыше 300 м²", image: '' },
+          { title: "до 100 м²", image: "" },
+          { title: "от 100 м² до 200 м²", image: "" },
+          { title: "от 200 м² до 300 м²", image: "" },
+          { title: "свыше 300 м²", image: "" },
         ],
       },
       {
@@ -51,27 +53,33 @@ export const useQuizStore = defineStore("quiz", {
         options: [
           {
             title: "Облицовочный кирпич",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
           {
             title: "Декоративная штукатурка",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
           {
             title: "Сайдинги",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
           {
             title: "Искусственный камень",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
           {
             title: "Фасадные панели",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
           {
             title: "Другой материал",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
         ],
       },
@@ -80,88 +88,103 @@ export const useQuizStore = defineStore("quiz", {
         options: [
           {
             title: "Черепица",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
           {
             title: "Фальцевая кровля",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
           {
             title: "Профнастил",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
           {
             title: "Наплавляемая кровля",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
           {
             title: "Мембрана",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
           {
             title: "Другой материал",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
         ],
       },
       {
         title: "Есть ли участок под строительство?",
-        options: [{ title: "Есть", image: '' }, { title: "Нет", image: '' }],
+        options: [
+          { title: "Есть", image: "" },
+          { title: "Нет", image: "" },
+        ],
       },
       {
         title: "Что бы вы хотели в дополнение к своему дому?",
         options: [
           {
             title: "Терраса",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
           {
             title: "Ландшафтный дизайн ",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
           {
             title: "Забор",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
           {
             title: "Беседка",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
           {
             title: "Баня",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
           {
             title: "Другое",
-            image: "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
+            image:
+              "https://avatars.mds.yandex.net/i?id=12f5786e1ccb3a0f35ad9f75a4b84dd2_l-9044040-images-thumbs&n=13",
           },
         ],
       },
       {
         title: "Какой бюджет:",
         options: [
-          { title: "до 6 млн ", image: '' },
-          { title: "от 6 млн до 9 млн", image: '' },
-          { title: "от 9 млн до 12 млн", image: '' },
-          { title: "от 12 млн до 15 млн", image: '' },
-          { title: "свыше 15 млн", image: '' },
+          { title: "до 6 млн ", image: "" },
+          { title: "от 6 млн до 9 млн", image: "" },
+          { title: "от 9 млн до 12 млн", image: "" },
+          { title: "от 12 млн до 15 млн", image: "" },
+          { title: "свыше 15 млн", image: "" },
         ],
       },
       {
         title: "Когда планируете строительство?",
         options: [
-          { title: "в течение 2-3 мес", image: '' },
-          { title: "в течение полугода", image: '' },
-          { title: "в течение года", image: '' },
-          { title: "еще не определился ", image: '' },
+          { title: "в течение 2-3 мес", image: "" },
+          { title: "в течение полугода", image: "" },
+          { title: "в течение года", image: "" },
+          { title: "еще не определился ", image: "" },
         ],
       },
       {
         title: "При заказе дома от 100 м²",
         options: [
-          { title: "Биосептик", image: '' },
-          { title: "Дизайн проект дома", image: '' },
-          { title: "2% Скидка", image: '' },
+          { title: "Биосептик", image: "" },
+          { title: "Дизайн проект дома", image: "" },
+          { title: "2% Скидка", image: "" },
         ],
       },
       {
@@ -195,7 +218,7 @@ export const useQuizStore = defineStore("quiz", {
     },
 
     updateCurrentStep(step: number) {
-      this.currentStep = step; 
+      this.currentStep = step;
     },
 
     nextStep() {
