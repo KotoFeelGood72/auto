4187905e804afd061ece5916f5da345fe0e52e55
KotoFeelGood72 <kotenkolove@yandex.ru>
2024-10-22 12:14:05 +0300

    ss

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL_ASSET),
   scrollBehavior(to: any, from: any, savedPosition: any) {
     if (to.path === from.path && to.fullPath !== from.fullPath) {
       return null;
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || "/",
+  base: process.env.VITE_BASE_URL_ASSET || "/",
   build: {
     rollupOptions: {
       output: {
