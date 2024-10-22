3c5ce215bdbfd087e30de215c6374afa894e49e3
KotoFeelGood72 <kotenkolove@yandex.ru>
2024-10-22 09:00:21 +0300

    Поправил ENV

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api/",
+  baseURL:
+    import.meta.env.MODE === "production"
+      ? "http://2klstk.ru/wp-content/uploads/json/" // Прямой путь для продакшена
+      : "/api/", // Путь для локальной разработки с проксированием
 });
 
 export { api };
