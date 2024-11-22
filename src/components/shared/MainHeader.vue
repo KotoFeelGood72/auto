002fe11932846e84c5c79985aefdeb76146a174d
KotoFeelGood72 <kotenkolove@yandex.ru>
2024-11-22 21:35:56 +0300

    s

--- src/components/shared/MainHeader.vue
+++ src/components/shared/MainHeader.vue
@@ -8,9 +8,7 @@
           </RouterLink>
           <div class="header__logo_cta">
             <h2 class="header__logo_title">Технологии комфорта</h2>
-            <div class="header__logo_descr">
-              Строительство качественных домов
-            </div>
+            <div class="header__logo_descr">Строительство качественных домов</div>
           </div>
         </div>
         <a
@@ -19,9 +17,7 @@
           target="_blank"
         >
           <div class="header__yandex_img">
-            <img
-              src="https://2klstk.ru/wp-content/uploads/2024/09/yandex.png"
-            />
+            <img src="https://2klstk.ru/wp-content/uploads/2024/09/yandex.png" />
           </div>
           На основании 50 отзывов</a
         >
@@ -45,9 +41,7 @@
           <div class="header__work_time">
             <div class="header__date">Без выходных 9:00-18:00</div>
             <a class="header__phone" href="tel:+79282755456">+7 928 275-54-56</a
-            ><a class="header__call_us" @click="openModal('form')"
-              >Заказать звонок</a
-            >
+            ><a class="header__call_us" @click="openModal('form')">Заказать звонок</a>
           </div>
         </div>
         <button
@@ -63,11 +57,7 @@
         <nav class="header__nav">
           <div class="header__nav_in">
             <ul class="header__nav_list">
-              <li
-                class="menu-item"
-                v-for="(item, i) in menu"
-                :key="'menu-item-' + i"
-              >
+              <li class="menu-item" v-for="(item, i) in menu" :key="'menu-item-' + i">
                 <a :href="item.link">{{ item.name }}</a>
               </li>
             </ul>
@@ -78,9 +68,7 @@
                 target="_blank"
               >
                 <div class="header__yandex_img">
-                  <img
-                    src="https://2klstk.ru/wp-content/uploads/2024/09/yandex.png"
-                  />
+                  <img src="https://2klstk.ru/wp-content/uploads/2024/09/yandex.png" />
                 </div>
                 На основании 50 отзывов</a
               >
@@ -90,11 +78,7 @@
                   v-for="(item, i) in socials"
                   :key="'mobile-socials-item' + i"
                 >
-                  <a
-                    class="header__social_link"
-                    :href="item.link"
-                    target="_blank"
-                  >
+                  <a class="header__social_link" :href="item.link" target="_blank">
                     <img :src="item.img" />
                   </a>
                 </li>
@@ -106,8 +90,7 @@
               >
               <div class="header__work_time">
                 <div class="header__date">Без выходных 9:00-18:00</div>
-                <a class="header__phone" href="tel:+79282755456"
-                  >+7 928 275-54-56</a
+                <a class="header__phone" href="tel:+79282755456">+7 928 275-54-56</a
                 ><a class="header__call_us" href="#" @click="openModal('form')"
                   >Заказать звонок</a
                 >
@@ -133,7 +116,8 @@ const socials = ref([
     link: "https://wtsp.cc/79282755456",
   },
   {
-    img: "https://2klstk.ru/wp-content/themes/comfort/assets/images/contact/mail-social.svg",
+    img:
+      "https://2klstk.ru/wp-content/themes/comfort/assets/images/contact/mail-social.svg",
     link: "mailto:2klstk@mail.ru",
   },
 ]);
@@ -179,6 +163,12 @@ onUnmounted(() => {
   }
 
   @media (max-width: 1372px) {
+    display: none;
+  }
+}
+
+.header__social_list {
+  @media (max-width: 1502px) {
     display: none;
   }
 }
